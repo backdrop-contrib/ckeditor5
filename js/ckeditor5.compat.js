71bcc35ba4b32334ce6bd20d6909b02d16abd701
indigoxela <user@olga.indigofloat.at>
2023-08-28 17:21:00 +0200

    Fix wording a bit

--- js/ckeditor5.compat.js
+++ js/ckeditor5.compat.js
@@ -5,7 +5,7 @@
  * When both libraries are loaded, the basePath determination of CKEditor4
  * fails regularly. This variable is only used by v4 and prevents breaking init,
  * when the attached method has not run yet. For example, when both editor
- * versions in the "Formatting options" and v5 is the default.
+ * versions are available in "Formatting options" and v5 is the default.
  *
  * This workaround does not prevent all problems with CKEDITOR's
  * resourceManager.load, but the editor will successfully initialize after
